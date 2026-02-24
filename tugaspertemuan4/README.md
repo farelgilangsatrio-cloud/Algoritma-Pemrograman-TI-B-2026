e167f4c0f8c8beefa84191495c2385a86936a033
farelgilangsatrio-cloud <farelgilangsatrio@gmail.com>
2026-02-24 19:29:12 +0700

    commit tugas pertemuan 4

--- tugaspertemuan4/README.md
+++
@@ -1,2 +0,0 @@
-# Algoritma-Pemrograman-TI-B-2026
-Repository ini adalah repository untuk mata kuliah praktikum Algoritma Pemrograman Teknik Informatika Kelas B Tahun Ajaran 2026
--- tugaspertemuan4/tugas2/soal4.py
+++
@@ -1,8 +0,0 @@
-# soal 4
-def pangkat_rekursif(a, b):
-    if b == 0:
-        return 1
-    else:
-        return a * pangkat_rekursif(a, b - 1)
-    
-print("Hasil 2 pangkat 5:", pangkat_rekursif(2, 5))
